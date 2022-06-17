1b361ee5d0c6361db6047d06c18905f4486ddbef
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-17 08:07:28 +0200

    162. Task: Aditional properties and controls

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -9,15 +9,25 @@ export interface MyLabelProps {
      * Size of the label
      */
     size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * Capitalize the label
+     */
+    allCaps?: boolean;
+    /**
+     * Color of the label
+     */
+    color?: 'primary' | 'secondary' | 'tertiary';
 }
 
 export const MyLabel = ({
+    allCaps = false,
+    color = 'primary',
     label = 'No Label',
     size = 'normal',
 }: MyLabelProps) => {
-  return (
-    <span className={`${ size }`}>
-        { label }
-    </span>
-  )
+    return (
+        <span className={`label ${ size } text-${ color }`}>
+            { allCaps ? label.toUpperCase() : label }
+        </span>
+    )
 }
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -5,6 +5,10 @@ import { MyLabel } from '../../components/MyLabel';
 export default {
     title: 'UI/MyLabel',
     component: MyLabel,
+    argTypes: {
+        size: { control: 'select' },
+        color: { control: 'select' },
+    }
 } as ComponentMeta<typeof MyLabel>
 
 const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel { ...args } />;
@@ -13,8 +17,30 @@ export const Basic = Template.bind({});
 Basic.args = {
     size: 'normal',
     label: 'No Label',
+    allCaps: false,
+    color: 'primary'
 }
 
 export const AllCaps = Template.bind({});
+AllCaps.args = {
+    size: 'normal',
+    label: 'No Label',
+    allCaps: true,
+    color: 'primary'
+}
 
 export const Secondary = Template.bind({});
+Secondary.args = {
+    size: 'normal',
+    label: 'No Label',
+    allCaps: false,
+    color: 'secondary'
+}
+
+export const Tertieary = Template.bind({});
+Tertieary.args = {
+    size: 'normal',
+    label: 'No Label',
+    allCaps: false,
+    color: 'tertiary'
+}
